9dac245eae4a228fd422405cd6837183c33622b4
[liuwenbin] <[liuwbt@yonyou.com]>
2019-02-16 21:37:20 +0800

    cors finished

---
+++ spring-boot-cors/spring-boot-cors-provider/src/main/java/com/roncoo/education/SpringBootDemo112Application.java
@@ -0,0 +1,14 @@
+package com.roncoo.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class SpringBootDemo112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo112Application.class, args);
+	}
+}
---
+++ spring-boot-cors/spring-boot-cors-provider/src/main/java/com/roncoo/education/controller/ApiController.java
@@ -0,0 +1,28 @@
+/**
+ * 2015-2016 龙果学院 (www.roncoo.com)
+ */
+package com.roncoo.education.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+/**
+ * @author wujing
+ */
+@RestController
+@RequestMapping(value = "/api", method = RequestMethod.POST)
+public class ApiController {
+
+	// @CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/get")
+	public HashMap<String, Object> get(@RequestParam String name) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("title", "hello world");
+		map.put("name", name);
+		return map;
+	}
+}
