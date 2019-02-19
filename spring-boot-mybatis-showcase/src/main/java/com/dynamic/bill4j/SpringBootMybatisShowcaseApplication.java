3ac852fadd7efe36e84d1fce2e3929317b16f2af
[liuwenbin] <[liuwbt@yonyou.com]>
2019-02-19 08:27:08 +0800

    mybatis with spring-boot2.0

---
+++ spring-boot-mybatis-showcase/src/main/java/com/dynamic/bill4j/SpringBootMybatisShowcaseApplication.java
@@ -0,0 +1,16 @@
+package com.dynamic.bill4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bill
+ */
+@SpringBootApplication
+public class SpringBootMybatisShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisShowcaseApplication.class, args);
+	}
+
+}
